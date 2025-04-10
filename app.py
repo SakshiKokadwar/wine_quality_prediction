daa7452b33129fac0fd6ded7e90267ae1a6d8d2e
Sakshi Kokadwar <sakshikokadwar03@gmail.com>
2025-04-10 20:55:15 +0530

    first commit

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Wine Quality: {result}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=10000)
